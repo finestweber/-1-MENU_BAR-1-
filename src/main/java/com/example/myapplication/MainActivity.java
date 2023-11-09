65a2d3c3ebadf0d8828df73ee1a3bbdcec4967d6
Gamaliel Pereira <finestweber@gmail.com>
2023-11-08 23:06:49 -0300

    [1]MENU_BAR[1]

--- src/main/java/com/example/myapplication/MainActivity.java
+++ src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //[2] Evento de Cluque TOAST
+        Button btn = (Button)findViewById(R.id.botao);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"bem Vindo ao meu primeiro Toast",Toast.LENGTH_SHORT).show();
+            }
+        });
+        //[2] Evento de clique TOAST
     }
 //[1]codigo que puxa o menu
     @Override
